906de5d7af254208b22c11f59d07c2a676cea5af
Igi <brad@igi.codes>
2019-09-07 00:27:57 +0100

    Cleanup

--- Inventory.Client/InventoryService.cs
+++ Inventory.Client/InventoryService.cs
@@ -6,9 +6,7 @@ using NFive.SDK.Client.Rpc;
 using NFive.SDK.Client.Services;
 using NFive.SDK.Core.Diagnostics;
 using NFive.SDK.Core.Models.Player;
-using System;
 using System.Threading.Tasks;
-using IgiCore.Inventory.Client.Overlays;
 using IgiCore.Inventory.Shared;
 
 namespace IgiCore.Inventory.Client
@@ -17,7 +15,6 @@ namespace IgiCore.Inventory.Client
 	public class InventoryService : Service
 	{
 		private Configuration config;
-		private InventoryOverlay overlay;
 
 		public InventoryService(ILogger logger, ITickManager ticks, IEventManager events, IRpcHandler rpc, ICommandManager commands, OverlayManager overlay, User user) : base(logger, ticks, events, rpc, commands, overlay, user) { }
 
@@ -26,24 +23,8 @@ namespace IgiCore.Inventory.Client
 			// Request server configuration
 			this.config = await this.Rpc.Event(InventoryEvents.Configuration).Request<Configuration>();
 
-			this.Logger.Debug($"From server config: {this.config.Example}");
-
 			// Update local configuration on server configuration change
 			this.Rpc.Event(InventoryEvents.Configuration).On<Configuration>((e, c) => this.config = c);
-
-			// Create overlay
-			this.overlay = new InventoryOverlay(this.OverlayManager);
-
-			// Attach a tick handler
-			this.Ticks.Attach(OnTick);
-		}
-
-		private async Task OnTick()
-		{
-			this.Logger.Debug("Hello World!");
-			// Do something every frame
-
-			await Delay(TimeSpan.FromSeconds(1));
 		}
 	}
 }
