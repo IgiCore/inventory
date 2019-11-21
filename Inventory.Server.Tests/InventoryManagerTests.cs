4f78c09dd4e404f06ea1233c9e0aa53ee060a3c7
Brad Park <brad.park@apdcomms.co.uk>
2019-11-21 16:42:52 +0000

    Fixed MSTest references

--- Inventory.Server.Tests/InventoryManagerTests.cs
+++ Inventory.Server.Tests/InventoryManagerTests.cs
@@ -1,8 +1,6 @@
 using System;
-using System.Collections;
 using System.Collections.Generic;
 using System.Data.Entity;
-using System.Linq;
 using IgiCore.Inventory.Server.Models;
 using IgiCore.Inventory.Server.Storage;
 using IgiCore.Inventory.Shared.Exceptions;
